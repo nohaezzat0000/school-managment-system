31478ee110357523839b3902c6c605d0a7919323
nohaezzat0000 <nohaezzat1100@gmail.com>
2025-07-19 21:41:49 +0300

    edit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { providePrimeNG } from 'primeng/config';
 
 import { routes } from './app.routes';
+import { PRIME_NG_MODULES } from './primeng.config';
+import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimations(),
+    importProvidersFrom(...PRIME_NG_MODULES),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Products } from './components/products/products';
+import { Cart } from './components/cart/cart';
+import { Contact } from './components/contact/contact';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: Home},
+    { path: 'products', component: Products},
+    { path: 'cart', component: Cart},
+    { path: 'contact', component: Contact}
+
+];
