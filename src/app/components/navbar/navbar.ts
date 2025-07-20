6105670c17987100ac8da42a1ab635a27b1afc57
Ahmed-Fa1sal <DerRiese765@gmail.com>
2025-07-20 15:48:41 +0300

    Added translation

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,23 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { MenubarModule } from 'primeng/menubar';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MenubarModule],
+  imports: [MenubarModule, ButtonModule, TranslateModule],
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.css']
 })
-export class Navbar {
+export class Navbar implements OnInit {
   items: MenuItem[] = [];
+  currentLang = 'en';
+
+  constructor(private translate: TranslateService) {}
 
   ngOnInit() {
-    this.items = [
-      { label: 'Home', icon: 'pi pi-home', routerLink: '/' },
-      { label: 'Products', icon: 'pi pi-box', routerLink: '/products' },
-      { label: 'Contact Us', icon: 'pi pi-envelope', routerLink: '/contact' },
-      { label: 'Cart', icon: 'pi pi-shopping-cart', routerLink: '/cart' }
-    ];
+    // Set the default language
+    this.translate.use(this.currentLang);
+    this.setDocumentDirection();
+
+    // Load the translated labels for menu items
+    this.updateMenuItems();
+
+    // Update menu items when language changes
+    this.translate.onLangChange.subscribe(() => {
+      this.updateMenuItems();
+      this.setDocumentDirection();
+    });
+  }
+
+  updateMenuItems() {
+    this.translate.get([
+      'navbar.home',
+      'navbar.products',
+      'navbar.contact',
+      'navbar.cart'
+    ]).subscribe(translations => {
+      this.items = [
+        { label: translations['navbar.home'], icon: 'pi pi-home', routerLink: '/' },
+        { label: translations['navbar.products'], icon: 'pi pi-box', routerLink: '/products' },
+        { label: translations['navbar.contact'], icon: 'pi pi-envelope', routerLink: '/contact' },
+        { label: translations['navbar.cart'], icon: 'pi pi-shopping-cart', routerLink: '/cart' }
+      ];
+    });
+  }
+
+  toggleLanguage() {
+    this.currentLang = this.currentLang === 'en' ? 'ar' : 'en';
+    this.translate.use(this.currentLang);
+    this.setDocumentDirection();
+  }
+
+  setDocumentDirection() {
+    document.documentElement.dir = this.currentLang === 'ar' ? 'rtl' : 'ltr';
   }
 }
