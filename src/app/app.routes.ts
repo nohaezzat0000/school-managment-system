5dd1114e40dd0a34fef80f9955ced7e909c4d57b
lena <lenaalsehli@yahoo.com>
2025-07-21 10:05:26 +0300

    auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { Home } from './components/home/home';
 import { Products } from './components/products/products';
 import { Cart } from './components/cart/cart';
 import { Contact } from './components/contact/contact';
+import {Auth} from './components/auth/auth';
 
 
 export const routes: Routes = [
     { path: '', component: Home},
     { path: 'products', component: Products},
     { path: 'cart', component: Cart},
-    { path: 'contact', component: Contact}
+    { path: 'contact', component: Contact},
+    {path: 'auth' , component: Auth}
 
 ];
---
+++ src/app/components/auth/auth.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MessageService } from 'primeng/api';
+import {Toast} from 'primeng/toast';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-auth',
+  providers: [MessageService],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    Toast
+  ],
+  templateUrl: './auth.html',
+  styleUrl: './auth.css'
+})
+export class Auth {
+  activeTab: 'signin' | 'signup' = 'signin';
+  signInForm: FormGroup;
+  signUpForm: FormGroup;
+
+  constructor(private fb: FormBuilder,
+              private messageService: MessageService,
+              private router: Router
+  ) {
+    this.signInForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+
+    this.signUpForm = this.fb.group({
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSignIn() {
+    if (this.signInForm.invalid) {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'Invalid login credentials'
+      });
+    } else {
+      this.router.navigate(['/dashboard']); // only for now we will validate it later
+    }
+  }
+
+  onSignUp() {
+    if (this.signUpForm.valid) {
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Success',
+        detail: 'Please check your email to verify your account.',
+      });
+    }
+  }
+
+  switchTab(tab: 'signin' | 'signup') {
+    this.activeTab = tab;
+  }
+}
