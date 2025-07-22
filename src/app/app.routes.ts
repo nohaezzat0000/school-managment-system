651de450357923f4fc1e2cd24d05d4a0d3ceaeed
nohaezzat0000 <nohaezzat1100@gmail.com>
2025-07-22 21:37:20 +0300

    refactor code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
-import { Products } from './components/products/products';
-import { Cart } from './components/cart/cart';
-import { Contact } from './components/contact/contact';
 import {Auth} from './components/auth/auth';
 import { Management } from './components/management/management';
 
 
 export const routes: Routes = [
     { path: '', component: Home},
-    { path: 'products', component: Products},
-    { path: 'cart', component: Cart},
-    { path: 'contact', component: Contact},
     {path: 'auth' , component: Auth},
     {path: 'management', component: Management}
 
