9433ea0886131c4a07c4f6b7337718104517961d
Reham-Alsaedi <Rehamalsaedi22@gmail.com>
2025-07-29 14:58:50 +0300

    CommonService + APIurls

---
+++ src/app/appCommon/ApisUrl/api-ulrs.ts
@@ -0,0 +1,11 @@
+/*import { environment } from 'src/environments/environment';
+const BASE_URL = environment.apiBaseUrl;*/
+
+export const ApiUlrs ={
+
+  //USERS: `${BASE_URL}/users`,
+  //LOGIN: `${BASE_URL}/auth/login`,  //--> later in enviroment
+  USERS: '',
+  LOGIN: '',
+
+}
---
+++ src/app/services/CommonService/common-service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommonService {
+
+  constructor(private http: HttpClient) {}
+
+  getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token') || '';
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    });
+  }
+
+  public postObservable<T>(
+    url: string,
+    requestBody: any,
+    headers?: HttpHeaders,
+    responseType: 'json' = 'json',
+    options?: any
+  ): Observable<T> {
+    const mergedHeaders = headers || this.getHeaders();
+    return this.http.post<any>(url, requestBody, {
+      headers: mergedHeaders,
+      responseType,
+      ...options
+    }).pipe(
+      map((data: any) => {
+        // errors handling
+        return data;
+      })
+    );
+  }
+
+  public getObservable<T>(
+    url: string,
+    headers?: HttpHeaders
+  ): Observable<T> {
+    const mergedHeaders = headers || this.getHeaders();
+    return this.http.get<any>(url, { headers: mergedHeaders }).pipe(
+      map((data: any) => {
+        // errors handling
+        return data;
+      })
+    );
+  }
+}
