8ef3c61178c4fad39cd88a6d0b41b42279bb33dc
Reham-Alsaedi <rehamalsaedi22@gmail.com>
2025-07-31 16:50:41 +0300

    User Profile from mock API

--- src/app/shared/user-profile/user-profile.ts
+++ src/app/shared/user-profile/user-profile.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
@@ -7,6 +7,7 @@ import { InputTextModule } from 'primeng/inputtext';
 import { AvatarModule } from 'primeng/avatar';
 import { PanelModule } from 'primeng/panel';
 import { DividerModule } from 'primeng/divider';
+import { UserService } from '../../services/userService/user';
 
 @Component({
   selector: 'app-user-profile',
@@ -16,17 +17,33 @@ import { DividerModule } from 'primeng/divider';
   templateUrl: './user-profile.html',
   styleUrl: './user-profile.css'
 })
-export class UserProfile {
-  user = {
+export class UserProfile implements OnInit{
+  /*user = {
     firstName: ' John',
     lastName: ' Doe',
     email: ' john.doe@example.com',
     phone: ' 0551234567',
     birthdate: new Date('1995-04-12'),
     job: ' Software Developer'
-  };
-  onEdit() {
+  };*/
+
+  user: any = null;
+constructor( private userService: UserService) {
+}
+ngOnInit() {
+  this.userService.getUserProfile().subscribe({
+    next: (data) => {
+      data.birthdate = new Date(data.birthdate);
+      this.user = data;
+    },
+    error: (err) => {
+      console.error('Failed to load user profile:', err);
+    }
+  });
+}
+onEdit() {
     // Navigate to edit profile, or toggle edit mode
     console.log('Edit profile clicked');
-  }}
+  }
+}
 
